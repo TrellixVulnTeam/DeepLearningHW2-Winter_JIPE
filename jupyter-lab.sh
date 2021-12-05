df3634d39ea01cbcf17774a0691322574ec5ed20
danibondar <danibondar@gmail.com>
2021-12-05 22:42:42 +0200

    updated hw2 files

--- jupyter-lab.sh
+++ jupyter-lab.sh
@@ -8,23 +8,15 @@
 #
 # Example usage:
 #
-# To run on the gateway machine (limited resources, no GPU):
-# ./jupyter-lab.sh
+# 1. First, activate the relevant conda env
+# $ conda activate cs236781-hwN
 #
-# To run on a compute node:
-# srun -c 2 --gres=gpu:1 --pty jupyter-lab.sh
+# 2. Run this script on a compute node:
+# $ srun -c 2 --gres=gpu:1 --pty jupyter-lab.sh
 #
 
-###
-# Conda parameters
-#
-CONDA_HOME=$HOME/miniconda3
-CONDA_ENV=cs236781-hw
 
 unset XDG_RUNTIME_DIR
-source $CONDA_HOME/etc/profile.d/conda.sh
-conda activate $CONDA_ENV
 
-# jupyter lab --no-browser --ip=$(hostname -I) --port-retries=100
 xvfb-run -a -s "-screen 0 1440x900x24" jupyter lab --no-browser --ip=$(hostname -I) --port-retries=100
 

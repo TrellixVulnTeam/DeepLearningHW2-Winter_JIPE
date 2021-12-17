a795c67886b9b9d136ee2fe15bc8ed664c0d589f
danibondar <danibondar@gmail.com>
2021-12-17 16:46:05 +0200

    fix

--- hw2/experiments.py
+++ hw2/experiments.py
@@ -87,7 +87,7 @@ def cnn_experiment(
         early_stopping=3,
         checkpoints=None,
         lr=5e-3,
-        reg=3e-3,
+        reg=1e-3,
         # Model params
         filters_per_layer=[64],
         layers_per_block=2,
@@ -143,7 +143,7 @@ def cnn_experiment(
                         conv_params=dict(kernel_size=3, stride=1, padding=1),
                         activation_type='relu', pooling_type='avg', pooling_params=dict(kernel_size=2))
     )
-    momentum = 0.9
+    momentum = 1
     oprim_dict = dict(lr=lr, weight_decay=reg, momentum=momentum)
 
     optimizer = torch.optim.SGD(params=model.parameters(), **oprim_dict)

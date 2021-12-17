a79367d1b7c247c4575c2494c6590ced439e145d
danibondar <danibondar@gmail.com>
2021-12-17 14:55:14 +0200

    fix

--- hw2/experiments.py
+++ hw2/experiments.py
@@ -131,22 +131,23 @@ def cnn_experiment(
     #   for you automatically.
     fit_res = None
     # ====== YOUR CODE: ======
-    '''
     dl_train = torch.utils.data.DataLoader(ds_train, batches, shuffle=False)
     dl_test = torch.utils.data.DataLoader(ds_test, batches, shuffle=False)
     channels = []
     for layersPerBlock in range(layers_per_block):
         for filterPerLayer in filters_per_layer:
             channels.append(filterPerLayer)
-
+    print(f'{channels=}')
+    print(f'{[channel for channel in filters_per_layer for _ in range(layers_per_block)]}')
+    print(f'{ds_train[0][0].shape}')
+    x0, _ = ds_train[0]
+    print(f'{x0.shape}')
     model = ArgMaxClassifier(
         model=model_cls(ds_train[0][0].shape, 10, channels=channels,
                         pool_every=pool_every, hidden_dims=hidden_dims,
                         conv_params=dict(kernel_size=3, stride=1, padding=1),
                         activation_type='relu', pooling_type='avg', pooling_params=dict(kernel_size=2))
     )
-    lr = 5e-2
-    weight_decay = 0.01
     momentum = 0.9
     oprim_dict = dict(lr=lr, weight_decay=reg, momentum=momentum)
 
@@ -156,6 +157,8 @@ def cnn_experiment(
     trainer = ClassifierTrainer(model, loss_fn, optimizer, device=device)
     fit_res = trainer.fit(dl_train=dl_train, dl_test=dl_test, num_epochs=epochs, checkpoints=checkpoints,
                           early_stopping=early_stopping, **kw)
+    fit_res.test_acc = [test_acc_epoch.item() for test_acc_epoch in fit_res.test_acc]
+    fit_res.train_acc = [train_acc_epoch.item() for train_acc_epoch in fit_res.train_acc]
     '''
     dl_train = torch.utils.data.DataLoader(ds_train, batch_size=batches,
                                            shuffle=False)
@@ -176,6 +179,9 @@ def cnn_experiment(
     trainer = ClassifierTrainer(model, loss_fn, optimizer, device=device)
     fit_res = trainer.fit(dl_train=dl_train, dl_test=dl_test, num_epochs=epochs, checkpoints=checkpoints,
                           early_stopping=early_stopping, **kw)
+    fit_res.test_acc = [test_acc_epoch.item() for test_acc_epoch in fit_res.test_acc]
+    fit_res.train_acc = [train_acc_epoch.item() for train_acc_epoch in fit_res.train_acc]
+    '''
     # ========================
 
     save_experiment(run_name, out_dir, cfg, fit_res)
